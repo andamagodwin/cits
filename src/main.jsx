6555f69bb2d668f5556b1465ef5e67239680943b
andamagodwin <andamagodwinezra@gmail.com>
2025-04-02 20:41:10 +0300

    New: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+/**
+ * Components
+ */
+import App from './App.jsx';
+
+import './index.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
