39c776c8e6b23e915e701121508da11583f297e8
andamagodwin <andamagodwinezra@gmail.com>
2025-04-08 17:31:40 +0300

    New: added some minor changes

--- src/components/onboardingForm.tsx
+++ src/components/onboardingForm.tsx
@@ -4,6 +4,7 @@ const OnboardingForm = () => {
     return (
         <div>
             <h1>Onboarding Form</h1>
+            <h1>The content here is for the onboarding process</h1>
         </div>
     );
 };
