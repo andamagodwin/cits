5f5939ddb4f2dde1a0ac3d111a750f3da68d52a8
andamagodwin <andamagodwinezra@gmail.com>
2025-04-11 15:45:29 +0300

    New: added a step three

---
+++ src/components/steps/step-three.jsx
@@ -0,0 +1,10 @@
+const StepThree = ()=>{
+    return (
+        <div>
+            <h1>Step three component</h1>
+            <h2>Content of the step three component</h2>
+        </div>
+    )
+}
+
+export default StepThree
